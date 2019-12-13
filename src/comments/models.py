37c3fff6995dd5f97b56c2cb7bda4cf3dba68852
buckldav <runalldayandnight@gmail.com>
2019-12-13 09:22:22 -0700

    Add requirements

--- src/comments/models.py
+++ src/comments/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+  pass
+
 class Comment(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   comment = models.TextField()
   name = models.CharField(max_length=100)
+
+  _post = models.ForeignKey(to=Posts, on_delete=models.CASCADE)
   
   class Meta:
     ordering = ['created']
